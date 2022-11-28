cfdcbf2117dfca0203ececbf0a55bfabe6e2f44e
Valacsai <viktor.valacsai@gmail.com>
2022-11-28 18:58:28 +0100

    implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from './components';
 import styles from './style';
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stat Business Billing CardDeal Testimonials Clients CTA Footer
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{' '}
+          <Clients /> <CTA /> <Footer />
+          Hello
         </div>
       </div>
     </div>
